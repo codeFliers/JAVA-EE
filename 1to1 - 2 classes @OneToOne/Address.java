4ef0037e593dbd8ee60fc2ddc7cd6de867e64755
codeFliers <58827656+codeFliers@users.noreply.github.com>
2021-09-27 11:18:44 +0200

    1to1 - 2 classes @OneToOne

---
+++ 1to1 - 2 classes @OneToOne/Address.java
@@ -0,0 +1,45 @@
+package com.example.jpaProject;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name="addresses")
+public class Address implements Serializable {
+    @Id
+    private int identifiant;
+
+    @Column(name="rue", nullable = false, length=255)
+    private String rue;
+    @Column(name="codePostal", nullable = false, length=5)
+    private int codePostal;
+    @Column(name="ville", nullable = false, length=255)
+    private String ville;
+
+    public Address(){}
+
+    public String getRue() {
+        return rue;
+    }
+    public void setRue(String rue) {
+        this.rue = rue;
+    }
+
+
+    public int getCodePostal() {
+        return codePostal;
+    }
+    public void setCodePostal(int codePostal) {
+        this.codePostal = codePostal;
+    }
+
+
+    public String getVille() {
+        return ville;
+    }
+    public void setVille(String ville) {
+        this.ville = ville;
+    }
+
+
+}
